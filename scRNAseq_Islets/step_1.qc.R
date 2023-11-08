5d919b9fd0045a92c02a8458224cc0a28843a005
Tuo Zhang <taz2008@med.cornell.edu>
2023-11-08 13:24:33 -0500

    initilize my_functions codes for scRNAseq_Islets data set

--- scRNAseq_Islets/step_1.qc.R
+++
@@ -1,4 +0,0 @@
-# step_1.qc.R
-# step 1: perform QC on cells
-# 
-
---
+++ src/scRNAseq_Islets/my_functions.R
@@ -0,0 +1,64 @@
+# my_functions.R
+# customized functions for processing data and plotting
+# 
+
+library(Seurat, quietly=T, warn.conflicts=F)
+library(dplyr, quietly=T, warn.conflicts=F)
+library(ggplot2, quietly=T, warn.conflicts=F)
+library(Matrix, quietly=T, warn.conflicts=F)
+library(scater, quietly=T, warn.conflicts=F)
+library(reshape2, quietly=T, warn.conflicts=F)
+library(pheatmap, quietly=T, warn.conflicts=F)
+library(magrittr, quietly=T, warn.conflicts=F)
+library(cowplot, quietly=T, warn.conflicts=F)
+library(R.utils, quietly=T, warn.conflicts=F)
+
+# read in raw counts data from a given sample
+my.Read10X <- function(tcountdir, tprefix=NULL){
+  # directory exists?
+  if (! dir.exists(tcountdir)){
+    print(paste("input raw counts folder does NOT exist:", tcountdir, sep=" "))
+    return(NULL)
+  }
+  # file exists?
+  tmat.file <- paste(tcountdir, "matrix.mtx.gz", sep="/")
+  tfnames.file <- paste(tcountdir, "features.tsv.gz", sep="/")
+  tbnames.file <- paste(tcountdir, "barcodes.tsv.gz", sep="/")
+  for (tf in c(tmat.file, tfnames.file, tbnames.file)){
+    if (! file.exists(tf)){
+      print(paste("input file does NOT exist:", tf))
+      return(NULL)
+    }
+  }
+  # extract counts matrix
+  cat(paste("Loading UMI counts table from", tcountdir, "..."))
+  tmat <- readMM(paste(tcountdir, "matrix.mtx.gz", sep="/"))
+  tfnames <- read.delim(paste(tcountdir, "features.tsv.gz", sep="/"), header=FALSE, stringsAsFactors=FALSE)
+  tbnames <- read.delim(paste(tcountdir, "barcodes.tsv.gz", sep="/"), header=FALSE, stringsAsFactors=FALSE)
+  # update column names (cell ids)
+  if (is.null(tprefix)){
+    colnames(tmat) = tbnames$V1
+  }
+  else{
+    colnames(tmat) = paste(tprefix, tbnames$V1, sep="_")
+  }
+  rownames(tmat) <- uniquifyFeatureNames(ID=tfnames$V1, names=tfnames$V2)
+  cat(" done.","\n")
+  return(tmat)
+}
+
+# merge raw read counts table collected from multiple samples, in a more efficient way
+my.MergeMatrix.v2 <- function(tmats){
+  cat("Merge raw UMI counts ")
+  tfunc <- function(x,y){
+    tres <- cbind(x,y[rownames(x),])
+    cat(".")
+    return(tres)
+  }
+  tmerged <- Reduce(f=tfunc, x=tmats)
+  # fill na with 0
+  tmerged[is.na(tmerged)] <- 0
+  cat(" done.")
+  return(tmerged)
+}
+
